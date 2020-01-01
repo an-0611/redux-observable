a13ab819f085d17d481cfe88b81e9a2e05ed0c00
an <an19940611@gmail.com>
2020-01-01 21:52:01 +0800

    update reference url

--- src/components/index.js
+++ src/components/index.js
@@ -59,6 +59,10 @@ class MainSection extends Component {
         {/* thunk: https://dev.to/markusclaus/fetching-data-from-an-api-using-reactredux-55ao */}
         {/* redux-middleware structure */}
         {/* http://hahow-recruit.herokuapp.com/frontend */}
+        {/* https://medium.com/@max80713/%E8%A9%B3%E8%A7%A3-redux-middleware-efd6a506357e */}
+        {/* https://redux.js.org/introduction/three-principles/ */}
+        {/* https://medium.com/enjoy-life-enjoy-coding/react-%E5%9C%A8-hooks-%E4%B8%AD%E4%BB%A5-usecontext-%E8%88%87-usereducer-%E5%AF%A6%E7%8F%BE-redux-3a8aa403d9e4 */}
+        {/* https://medium.com/@nightspirit622/%E8%A9%B2%E4%B8%8D%E8%A9%B2%E7%94%A8context-api-%E4%BE%86%E5%8F%96%E4%BB%A3-redux-4d7395d5c8da */}
       </div>
     );
   }
